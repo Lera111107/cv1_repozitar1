5ff2a938962ed6885a63c1a2b5644a29665b0d08
Valerija Hodolič <xhodolic@stuba.sk>
2023-02-21 15:49:17 +0100

    Pridanie otazky

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,6 +3,7 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello svete");
+        System.out.println("Ako ste dnes?");
     }
 
 }
